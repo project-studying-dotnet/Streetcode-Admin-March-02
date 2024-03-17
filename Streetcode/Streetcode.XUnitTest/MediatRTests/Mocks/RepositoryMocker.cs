49a2fddc89b97398675bbfd53d44384fb29d9a0e
Gleb Shpuryk <stalkergleb74@gmail.com>
2024-03-17 22:07:14 +0200

    test/11/12: get art by id handler tests was added

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
@@ -16,11 +16,9 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
 {
     internal class RepositoryMocker
     {
-        private readonly IRepositoryWrapper _repositoryWrapper;
-
         public static Mock<IRepositoryWrapper> GetArtRepositoryMock()
         {
-            var arts = new List<Art>() 
+            var arts = new List<Art>()
             {
                 new Art { Id = 1, Description = "First description", Title = "First title", ImageId = 1, Image = null },
                 new Art { Id = 2, Description = "Second description", Title = "Second title", ImageId = 2, Image = null },
@@ -33,6 +31,13 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
 
             mockRepo.Setup(x => x.ArtRepository.GetAllAsync(It.IsAny<Expression<Func<Art, bool>>>(), It.IsAny<Func<IQueryable<Art>, IIncludableQueryable<Art, object>>>()))
                 .ReturnsAsync(arts);
+
+            mockRepo.Setup(x => x.ArtRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<Art, bool>>>(), It.IsAny<Func<IQueryable<Art>, IIncludableQueryable<Art, object>>>()))
+                .ReturnsAsync((Expression<Func<Art, bool>> predicate, Func<IQueryable<Art>, IIncludableQueryable<Art, object>> include) =>
+                {
+                    return arts.FirstOrDefault(predicate.Compile());
+                });
+
 
             return mockRepo;
         }
