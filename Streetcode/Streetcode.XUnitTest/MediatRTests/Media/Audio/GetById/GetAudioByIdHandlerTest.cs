6ee3114d708c269e4add765291c7c2b3ec014392
Gleb Shpuryk <stalkergleb74@gmail.com>
2024-03-18 10:14:23 +0200

    test/11/12: get base audio tests was added.

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Media/Audio/GetById/GetAudioByIdHandlerTest.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Media/Audio/GetById/GetAudioByIdHandlerTest.cs
@@ -1,13 +1,12 @@
 ﻿using AutoMapper;
 using FluentAssertions;
 using Moq;
-using Streetcode.BLL.DTO.Media.Art;
-using Streetcode.BLL.DTO.Media.Audio;
 using Streetcode.BLL.Interfaces.BlobStorage;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.Mapping.Media;
-using Streetcode.BLL.MediatR.Media.Art.GetAll;
+using Streetcode.BLL.MediatR.Media.Art.GetById;
 using Streetcode.BLL.MediatR.Media.Audio.GetAll;
+using Streetcode.BLL.MediatR.Media.Audio.GetById;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 using Streetcode.XUnitTest.MediatRTests.Mocks;
 using System;
@@ -17,18 +16,18 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Streetcode.XUnitTest.MediatRTests.Media.Audio.GetBaseAudio
+namespace Streetcode.XUnitTest.MediatRTests.Media.Audio.GetById
 {
     // TESTED SUCCESSFULLY
-    // BLL -> MediatR -> Media -> Audio -> GetAll
-    public class GetAllAudiousHandlerTest
+    // BLL -> MediatR -> Media -> Audio -> GetById
+    public class GetAudioByIdHandlerTest
     {
         private readonly IMapper _mapper;
         private readonly Mock<IRepositoryWrapper> _mockRepository;
         private readonly Mock<ILoggerService> _mockLogger;
         private readonly Mock<IBlobService> _mockBlob;
 
-        public GetAllAudiousHandlerTest()
+        public GetAudioByIdHandlerTest()
         {
             _mockRepository = RepositoryMocker.GetAudiosRepositoryMock();
 
@@ -45,42 +44,42 @@ namespace Streetcode.XUnitTest.MediatRTests.Media.Audio.GetBaseAudio
         }
 
         [Fact]
-        public async Task Get_All_Not_Null_Or_Empty_Test()
+        public async Task Get_By_Id_Not_Null_Test()
         {
             //Arrange
-            var handler = new GetAllAudiosHandler(_mockRepository.Object, _mapper, _mockBlob.Object, _mockLogger.Object);
+            var handler = new GetAudioByIdHandler(_mockRepository.Object, _mapper, _mockBlob.Object, _mockLogger.Object);
 
             //Act
-            var result = await handler.Handle(new GetAllAudiosQuery(), CancellationToken.None);
+            var result = await handler.Handle(new GetAudioByIdQuery(2), CancellationToken.None);
 
             //Assert        
-            result.Value.Should().NotBeNullOrEmpty();
+            result.Value.Should().NotBeNull();
         }
 
         [Fact]
-        public async Task Get_All_Count_Should_Be_Four()
+        public async Task Get_By_Id_First_Should_Be_First_Test()
         {
             //Arrange
-            var handler = new GetAllAudiosHandler(_mockRepository.Object, _mapper, _mockBlob.Object, _mockLogger.Object);
+            var handler = new GetAudioByIdHandler(_mockRepository.Object, _mapper, _mockBlob.Object, _mockLogger.Object);
 
             //Act
-            var result = await handler.Handle(new GetAllAudiosQuery(), CancellationToken.None);
+            var result = await handler.Handle(new GetAudioByIdQuery(1), CancellationToken.None);
 
             //Assert        
-            result.Value.Count().Should().Be(4);
+            result.Value.MimeType.Should().Be("First mime type");
         }
 
         [Fact]
-        public async Task Get_All_Should_Be_Type_List_AudioDTO()
+        public async Task Get_By_Id_Second_Should_Not_Be_Fourth_Test()
         {
             //Arrange
-            var handler = new GetAllAudiosHandler(_mockRepository.Object, _mapper, _mockBlob.Object, _mockLogger.Object);
+            var handler = new GetAudioByIdHandler(_mockRepository.Object, _mapper, _mockBlob.Object, _mockLogger.Object);
 
             //Act
-            var result = await handler.Handle(new GetAllAudiosQuery(), CancellationToken.None);
+            var result = await handler.Handle(new GetAudioByIdQuery(2), CancellationToken.None);
 
             //Assert        
-            result.Value.Should().BeOfType<List<AudioDTO>>();
+            result.Value.MimeType.Should().NotBe("Fourth mime type");
         }
     }
 }
