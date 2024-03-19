8ab1a995c829207da3ff0f7c5a0655aad009dd84
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-03-20 01:04:14 +0200

    GetAllMain Handler Tests started

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Team/GetAllMainHandler.cs
@@ -0,0 +1,72 @@
+﻿namespace Streetcode.XUnitTest.MediatRTests.Team;
+
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Mapping.Team;
+using Streetcode.BLL.MediatR.Team.GetAll;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.XUnitTest.MediatRTests.Mocks;
+using Xunit;
+
+public class GetAllMainHandlerTest
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<IRepositoryWrapper> _mockRepository;
+    private readonly Mock<ILoggerService> _mockLogger;
+
+    public GetAllMainHandlerTest()
+    {
+        _mockRepository = RepositoryMocker.GetTeamRepositoryMockGorGetAll();
+
+        var mapperConfig = new MapperConfiguration(c =>
+        {
+            c.AddProfile<TeamProfile>();
+        });
+
+        _mapper = mapperConfig.CreateMapper();
+
+        _mockLogger = new Mock<ILoggerService>();
+    }
+
+    [Fact]
+    public async Task GetAll_ReturnsNotNullOrEmpty()
+    {
+        // Arrange
+        var handler = new GetAllMainTeamHandler(_mockRepository.Object, _mapper, _mockLogger.Object);
+
+        // Act
+        var result = await handler.Handle(new GetAllMainTeamQuery(), CancellationToken.None);
+
+        // Assert
+        result.Value.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task GetAll_ReturnsTypeOfTeamMemberDTO()
+    {
+        // Arrange
+        var handler = new GetAllMainTeamHandler(_mockRepository.Object, _mapper, _mockLogger.Object);
+
+        // Act
+        var result = await handler.Handle(new GetAllMainTeamQuery(), CancellationToken.None);
+
+        // Assert
+        result.Value.Should().BeOfType<List<TeamMemberDTO>>();
+    }
+
+    [Fact]
+    public async Task GetAll_CountShouldBeTwo()
+    {
+        // Arrange
+        var handler = new GetAllMainTeamHandler(_mockRepository.Object, _mapper, _mockLogger.Object);
+
+        // Act
+        var result = await handler.Handle(new GetAllMainTeamQuery(), CancellationToken.None);
+
+        // Assert
+        result.Value.Count().Should().Be(2);
+    }
+}
