f0926ed91b8b5d776483a2de0f9607a33abc2319
askochul <antonskochulyas@gmail.com>
2024-03-19 15:36:12 +0200

    Add GetAllPartnersHandlerTest() for Partners With 3 tests: Get_All_Not_Null_Or_Empty_Test() Get_All_Count_Should_Be_Four() Get_All_Should_Be_Type_List_PartnerDTO()

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
@@ -3,6 +3,7 @@ using Moq;
 using Repositories.Interfaces;
 using Streetcode.BLL.DTO.Media.Art;
 using Streetcode.DAL.Entities.Media.Images;
+using Streetcode.DAL.Entities.Partners;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 using Streetcode.DAL.Repositories.Realizations.Base;
 using System;
@@ -33,6 +34,30 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
 
             mockRepo.Setup(x => x.ArtRepository.GetAllAsync(It.IsAny<Expression<Func<Art, bool>>>(), It.IsAny<Func<IQueryable<Art>, IIncludableQueryable<Art, object>>>()))
                 .ReturnsAsync(arts);
+
+            return mockRepo;
+        }
+
+        public static Mock<IRepositoryWrapper> GetPartnersRepositoryMock()
+        {
+            var partners = new List<Partner>()
+            {
+                new Partner { Id = 1, Title = "First Title", LogoId = 1, IsKeyPartner = true, IsVisibleEverywhere = true, TargetUrl = "First Url", UrlTitle = "First Url Title", Description = "First Description", Logo = null },
+                new Partner { Id = 2, Title = "Second Title", LogoId = 2, IsKeyPartner = true, IsVisibleEverywhere = true, TargetUrl = "Second Url", UrlTitle = "Second Url Title", Description = "Second Description", Logo = null },
+                new Partner { Id = 3, Title = "Third Title", LogoId = 3, IsKeyPartner = true, IsVisibleEverywhere = true, TargetUrl = "Third Url", UrlTitle = "Third Url Title", Description = "Third Description", Logo = null },
+                new Partner { Id = 4, Title = "Fourth Title", LogoId = 4, IsKeyPartner = true, IsVisibleEverywhere = true, TargetUrl = "Fourth Url", UrlTitle = "Fourth Url Title", Description = "Fourth Description", Logo = null },
+            };
+
+            var mockRepo = new Mock<IRepositoryWrapper>();
+
+            mockRepo.Setup(x => x.PartnersRepository.GetAllAsync(It.IsAny<Expression<Func<Partner, bool>>>(), It.IsAny<Func<IQueryable<Partner>, IIncludableQueryable<Partner, object>>>()))
+                .ReturnsAsync(partners);
+
+            mockRepo.Setup(x => x.PartnersRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<Partner, bool>>>(), It.IsAny<Func<IQueryable<Partner>, IIncludableQueryable<Partner, object>>>()))
+                .ReturnsAsync((Expression<Func<Partner, bool>> predicate, Func<IQueryable<Partner>, IIncludableQueryable<Partner, object>> include) =>
+                {
+                    return partners.FirstOrDefault(predicate.Compile());
+                });
 
             return mockRepo;
         }
