1b5feff7ab6407a78cb67084b676d292fde2c91a
Denys Klymovets <den.kli2511@gmail.com>
2024-03-19 20:33:01 +0200

    MediatRInstagramTest

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
@@ -2,6 +2,8 @@
 using Moq;
 using Repositories.Interfaces;
 using Streetcode.BLL.DTO.Media.Art;
+using Streetcode.BLL.Interfaces.Instagram;
+using Streetcode.DAL.Entities.Instagram;
 using Streetcode.DAL.Entities.Media.Images;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 using Streetcode.DAL.Repositories.Realizations.Base;
@@ -33,6 +35,25 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
 
             mockRepo.Setup(x => x.ArtRepository.GetAllAsync(It.IsAny<Expression<Func<Art, bool>>>(), It.IsAny<Func<IQueryable<Art>, IIncludableQueryable<Art, object>>>()))
                 .ReturnsAsync(arts);
+
+            return mockRepo;
+        }
+
+
+        public static Mock<IInstagramService> GetInstagramPostsMock()
+        {
+            var posts = new List<InstagramPost>()
+            {
+                new InstagramPost{ Id = "1", Caption = "1Caption", IsPinned = true, MediaType = "Image", MediaUrl = "1url", Permalink = "1permalink", ThumbnailUrl = "1thumbnailurl" },
+                new InstagramPost{ Id = "2", Caption = "2Caption", IsPinned = true, MediaType = "Image", MediaUrl = "2url", Permalink = "2permalink", ThumbnailUrl = "2thumbnailurl" },
+                new InstagramPost{ Id = "3", Caption = "3Caption", IsPinned = true, MediaType = "Image", MediaUrl = "3url", Permalink = "3permalink", ThumbnailUrl = "3thumbnailurl" },
+                new InstagramPost{ Id = "4", Caption = "4Caption", IsPinned = true, MediaType = "Image", MediaUrl = "4url", Permalink = "4permalink", ThumbnailUrl = "4thumbnailurl" },
+            };
+
+            var mockRepo = new Mock<IInstagramService>();
+
+            mockRepo.Setup(x => x.GetPostsAsync())
+                .ReturnsAsync(posts);
 
             return mockRepo;
         }
