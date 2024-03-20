53981c5944ee06a4fd81da22929f616495c82b8b
Gleb Shpuryk <stalkergleb74@gmail.com>
2024-03-20 16:02:14 +0200

    test/29 : task finished

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
@@ -10,6 +10,7 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
     using Streetcode.DAL.Entities.Media;
     using Streetcode.DAL.Entities.Media.Images;
     using Streetcode.DAL.Entities.Streetcode;
+    using Streetcode.DAL.Entities.Toponyms;
     using Streetcode.DAL.Repositories.Interfaces.Base;
 
     /// <summary>
@@ -204,6 +205,34 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
                 .ReturnsAsync((Expression<Func<StreetcodeArt, bool>> predicate, Func<IQueryable<StreetcodeArt>, IIncludableQueryable<StreetcodeArt, object>> include) =>
                 {
                     return streetcodeArts.FirstOrDefault(predicate.Compile());
+                });
+
+            return mockRepo;
+        }
+
+        /// <summary>
+        /// Mocks toponyms repository.
+        /// </summary>
+        /// <returns>Returns mocked repository. </returns>
+        public static Mock<IRepositoryWrapper> GetToponymsRepositoryMock()
+        {
+            var toponyms = new List<Toponym>
+            {
+               new Toponym() { Id = 1, Oblast = "First", StreetName = "First streetname" },
+               new Toponym() { Id = 2, Oblast = "Second", StreetName = "Second streetname" },
+               new Toponym() { Id = 3, Oblast = "Third", StreetName = "Third streetname" },
+               new Toponym() { Id = 4, Oblast = "Fourth", StreetName = "Fourth streetname" },
+            };
+
+            var mockRepo = new Mock<IRepositoryWrapper>();
+
+            mockRepo.Setup(x => x.ToponymRepository.GetAllAsync(It.IsAny<Expression<Func<Toponym, bool>>>(), It.IsAny<Func<IQueryable<Toponym>, IIncludableQueryable<Toponym, object>>>()))
+                .ReturnsAsync(toponyms);
+
+            mockRepo.Setup(x => x.ToponymRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<Toponym, bool>>>(), It.IsAny<Func<IQueryable<Toponym>, IIncludableQueryable<Toponym, object>>>()))
+                .ReturnsAsync((Expression<Func<Toponym, bool>> predicate, Func<IQueryable<Toponym>, IIncludableQueryable<Toponym, object>> include) =>
+                {
+                    return toponyms.FirstOrDefault(predicate.Compile());
                 });
 
             return mockRepo;
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Toponyms/GetAll/GetAllToponymsHandlerTest.cs
@@ -0,0 +1,83 @@
+﻿// <copyright file="GetAllToponymsHandlerTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Streetcode.XUnitTest.MediatRTests.Toponyms.GetAll
+{
+    using AutoMapper;
+    using FluentAssertions;
+    using Moq;
+    using Streetcode.BLL.DTO.Media.Art;
+    using Streetcode.BLL.DTO.Toponyms;
+    using Streetcode.BLL.Interfaces.Logging;
+    using Streetcode.BLL.Mapping.Toponyms;
+    using Streetcode.BLL.MediatR.Media.Art.GetAll;
+    using Streetcode.BLL.MediatR.Toponyms.GetAll;
+    using Streetcode.DAL.Entities.Toponyms;
+    using Streetcode.DAL.Repositories.Interfaces.Base;
+    using Streetcode.XUnitTest.MediatRTests.Mocks;
+    using Xunit;
+
+    /// <summary>
+    /// Tested successfully.
+    /// </summary>
+    public class GetAllToponymsHandlerTest
+    {
+        private readonly IMapper mapper;
+        private readonly Mock<IRepositoryWrapper> mockRepository;
+        private readonly Mock<ILoggerService> mockLogger;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetAllToponymsHandlerTest"/> class.
+        /// </summary>
+        public GetAllToponymsHandlerTest()
+        {
+            this.mockRepository = RepositoryMocker.GetToponymsRepositoryMock();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<ToponymProfile>();
+            });
+
+            this.mapper = mapperConfig.CreateMapper();
+
+            this.mockLogger = new Mock<ILoggerService>();
+        }
+
+        /// <summary>
+        /// Get all not null or empty test.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        [Fact]
+        public async Task GetAllNotNullOrEmptyTest()
+        {       
+            // Arrange
+            var handler = new GetAllToponymsHandler(this.mockRepository.Object, this.mapper, this.mockLogger.Object);
+           
+            // Act
+            var result = await handler.Handle(new GetAllToponymsQuery(new GetAllToponymsRequestDTO() { Title = "First streetname" }), CancellationToken.None);
+
+            // Assert
+            result.Value.Should().NotBeNull();
+        }
+
+
+        /// <summary>
+        /// Get all list should be type ArtDTO.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        [Fact]
+        public async Task GetAllShouldBeTypeListToponymDTO()
+        {
+            // Arrange
+            var handler = new GetAllToponymsHandler(this.mockRepository.Object, this.mapper, this.mockLogger.Object);
+
+            // Act
+            var result = await handler.Handle(new GetAllToponymsQuery(new GetAllToponymsRequestDTO() { Title = "First streetname" }), CancellationToken.None);
+
+            // Assert
+            result.Value.Toponyms.Should().BeOfType<List<ToponymDTO>>();
+        }
+    }
+}
