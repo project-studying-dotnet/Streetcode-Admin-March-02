feed666daae22f5c096e6660510a0beab5b7be9c
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-03-23 19:01:43 +0200

    Added delete handler and endpoint for Fact

--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.Dto.Streetcode.TextContent.Fact;
 using Streetcode.BLL.Dto.Team;
 using Streetcode.BLL.MediatR.Streetcode.Fact.Create;
+using Streetcode.BLL.MediatR.Streetcode.Fact.Delete;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetAll;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetById;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetByStreetcodeId;
@@ -33,5 +34,11 @@ public class FactController : BaseApiController
     public async Task<IActionResult> Create([FromBody] FactBaseDto position)
     {
         return HandleResult(await Mediator.Send(new CreateFactCommand(position)));
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        return HandleResult(await Mediator.Send(new DeleteFactCommand(id)));
     }
 }
