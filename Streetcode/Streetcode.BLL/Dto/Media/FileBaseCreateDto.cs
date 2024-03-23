dd7b7e9e469b777ae4c87f0561f3f0a8194d7004
askochul <antonskochulyas@gmail.com>
2024-03-23 17:56:01 +0200

    1. Change property attribute "Title"(max length = 150) to class "FileBaseCreateDto" 2. Change Max length of prop "Title" to 150, and max length of prop "Alt" to 400.

--- Streetcode/Streetcode.BLL/Dto/Media/FileBaseCreateDto.cs
+++ Streetcode/Streetcode.BLL/Dto/Media/FileBaseCreateDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Streetcode.BLL.Dto.Media;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Streetcode.BLL.Dto.Media;
 
 public class FileBaseCreateDto
 {
+    [MaxLength(150)]
     public string? Title { get; set; }
     public string? BaseFormat { get; set; }
     public string? MimeType { get; set; }
