767ed5195ff5a8d9f7ec68845ce994a0ee592006
askochul <antonskochulyas@gmail.com>
2024-03-23 14:28:11 +0200

    1)DAL 1.1)Create folder "Locations" in folder "Entities" 1.2)Create class "Location" in folder "Locations" 1.3)Create folder "Locations" in two folders "Repositories->Interfaces" and "Repositories->Realizations" 1.4)Create ILocationRepository interface in "Repositories->Interfaces->Locations" folder 1.5)Create LocationRepository class in "Repositories->Realizations->Locations" folder

---
+++ Streetcode/Streetcode.DAL/Entities/Locations/Location.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Streetcode.DAL.Entities.Locations
+{
+    [Table("locations", Schema = "locations")]
+    public class Location
+    {
+        public int Id { get; set; }
+        public string? Streetname { get; set; }
+        public int TableNumber { get; set; }
+    }
+}
