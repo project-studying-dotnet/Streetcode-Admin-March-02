a0aac748f16dff976606f9207ea934ac847c99c6
askochul <antonskochulyas@gmail.com>
2024-03-23 17:31:07 +0200

    Made Admin/History Map 1)DAL 1.1)Create folder "Locations" in folder "Entities" 1.2)Create class "Location" in folder "Locations" 1.3)Create folder "Locations" in two folders "Repositories->Interfaces" and "Repositories->Realizations" 1.4)Create ILocationRepository interface in "Repositories->Interfaces->Locations" folder 1.5)Create LocationRepository class in "Repositories->Realizations->Locations" folder
    2)BLL
    2.1)Create folder "Locations" in "Dto" folder
    2.2)Create class "LocationDto" in folder "Dto->Locations"
    2.3)Create folder "Locations" in "Mapping" folder
    2.4)Create map profile for "Location" and "LocationDto" classes in folder "Mapping->Locations"
    2.5)Create "Locations" folder in "MediatR" folder
    2.6)Create two folder in "MediatR->Locations" Create and Delete
    2.7)In folder "MediatR->Locations->Create" create and implement CreateLocationCommand and
    CreateLocationHandler
    2.8)In folder "MediatR->Locations->Delete" create and implement DeleteLocationCommand and
    DeleteLocationHandler
    
    3)Web Api
    3.1)Create "Locations" folder in "Controllers" folder
    3.2)Create "LocationController" in folder "Controllers->Locations"
    3.3)Inherit the BaseApiController class in LocationController
    3.4)Create and implement two methods in "LocationController" Create (POST) Delete (DELETE)
    3.5)Update currently migration
    
    4)XUnit+Moq
    4.1)Create folder "Locations" in "MediatRTests"
    4.2)Create two folders in "MediaRTests->Locations" Create and Delete
    4.3)Create class CreateLocationHandlerTest and implement tests for create location logic
    4.4)Create class DeleteLocationHandlerTest and implement tests for delete location logic

---
+++ Streetcode/Streetcode.BLL/Mapping/Locations/LocationProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Streetcode.BLL.Dto.Locations;
+using Streetcode.DAL.Entities.Locations;
+
+namespace Streetcode.BLL.Mapping.Locations
+{
+    public class LocationProfile : Profile
+    {
+        public LocationProfile()
+        {
+            CreateMap<Location, LocationDto>().ReverseMap();
+        }
+    }
+}
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Locations/Create/CreateLocationHandlerTest.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="CreateLocationHandlerTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Streetcode.XUnitTest.MediatRTests.Locations.Create
+{
+    using AutoMapper;
+    using FluentAssertions;
+    using Moq;
+    using Streetcode.BLL.Interfaces.Logging;
+    using Streetcode.BLL.Mapping.Locations;
+    using Streetcode.BLL.MediatR.Locations.Create;
+    using Streetcode.DAL.Repositories.Interfaces.Base;
+    using Streetcode.XUnitTest.MediatRTests.Mocks;
+    using Xunit;
+
+    /// <summary>
+    ///  Can not test.
+    /// </summary>
+    public class CreateLocationHandlerTest
+    {
+        private readonly Mock<IRepositoryWrapper> mockRepository;
+        private readonly IMapper mapper;
+        private readonly Mock<ILoggerService> mockLogger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateLocationHandlerTest"/> class.
+        /// </summary>
+        public CreateLocationHandlerTest()
+        {
+            this.mockRepository = RepositoryMocker.GetLocationsRepositoryMock();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<LocationProfile>();
+            });
+
+            this.mapper = mapperConfig.CreateMapper();
+
+            this.mockLogger = new Mock<ILoggerService>();
+        }
+
+        /// <summary>
+        /// Create not null and should be created test.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        [Fact]
+        public async Task CreateNotNullLocationMustBeCreated()
+        {
+            // Arrange
+            var handler = new CreateLocationHandler(this.mapper, this.mockRepository.Object, this.mockLogger.Object);
+
+            // Act
+            var result = await handler.Handle(
+                new CreateLocationCommand(new BLL.Dto.Locations.LocationDto()
+                {
+                    Id = 1,
+                    Streetname = "Created StreetName",
+                    TableNumber = 1,
+                }), CancellationToken.None);
+
+            // Assert
+            result.Value.Should().NotBeNull();
+        }
+    }
+}
