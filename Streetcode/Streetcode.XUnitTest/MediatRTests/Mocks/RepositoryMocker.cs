45d27a70c9625ac76ab8cce75a2e2e6a88428aa0
Danylo-Pavlusenko <danil2005oleg@gmail.com>
2024-03-23 20:31:54 +0200

    Added tests for fact

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Mocks/RepositoryMocker.cs
@@ -10,6 +10,7 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
     using Streetcode.BLL.Interfaces.Instagram;
     using Streetcode.BLL.Interfaces.Payment;
     using Streetcode.DAL.Entities.AdditionalContent;
+    using Streetcode.DAL.Entities.AdditionalContent.Coordinates;
     using Streetcode.DAL.Entities.AdditionalContent.Coordinates.Types;
     using Streetcode.DAL.Entities.Instagram;
     using Streetcode.DAL.Entities.Media;
@@ -18,6 +19,7 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
     using Streetcode.DAL.Entities.Payment;
     using Streetcode.DAL.Entities.Sources;
     using Streetcode.DAL.Entities.Streetcode;
+    using Streetcode.DAL.Entities.Streetcode.TextContent;
     using Streetcode.DAL.Entities.Team;
     using Streetcode.DAL.Entities.Timeline;
     using Streetcode.DAL.Entities.Toponyms;
@@ -667,6 +669,7 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
 
             return mockRepo;
         }
+
         public static Mock<IPaymentService> GetPaymentMock()
         {
             var merchantPaymentInfo = new MerchantPaymentInfo
@@ -688,7 +691,52 @@ namespace Streetcode.XUnitTest.MediatRTests.Mocks
                .ReturnsAsync(new InvoiceInfo("invoiceId", "pageUrl"));
 
             return mockService;
+        }
 
+        public static Mock<IRepositoryWrapper> GetFactRepositoryMock()
+        {
+            var facts = new List<Fact>()
+            {
+                new Fact { Id = 1, Title = "1", StreetcodeId = 1},
+                new Fact { Id = 2, Title = "2", StreetcodeId = 1},
+                new Fact { Id = 3, Title = "3", StreetcodeId = 1},
+                new Fact { Id = 4, Title = "4", StreetcodeId = 1},
+                new Fact { Id = 5, Title = "5", StreetcodeId = 2},
+                new Fact { Id = 6, Title = "6", StreetcodeId = 2},
+                new Fact { Id = 7, Title = "7", StreetcodeId = 2},
+                new Fact { Id = 8, Title = "8", StreetcodeId = 3},
+                new Fact { Id = 9, Title = "9", StreetcodeId = 3},
+            };
+
+            var mockRepo = new Mock<IRepositoryWrapper>();
+
+            mockRepo.Setup(x => x.FactRepository
+                .GetAllAsync(
+                    It.IsAny<Expression<Func<Fact, bool>>>(),
+                    It.IsAny<Func<IQueryable<Fact>, IIncludableQueryable<Fact, object>>>()))
+                .ReturnsAsync(facts);
+
+            mockRepo.Setup(x => x.FactRepository
+                .GetAllAsync(
+                    It.IsAny<Expression<Func<Fact, bool>>>(),
+                    It.IsAny<Func<IQueryable<Fact>, IIncludableQueryable<Fact, object>>>()))
+                .ReturnsAsync((
+                    Expression<Func<Fact, bool>> predicate, 
+                    Func<IQueryable<Fact>, IIncludableQueryable<Fact, object>> include) =>
+                {
+                    return facts.Where(predicate.Compile());
+                });
+
+            mockRepo.Setup(x => x.FactRepository
+                .GetFirstOrDefaultAsync(
+                    It.IsAny<Expression<Func<Fact, bool>>>(),
+                    It.IsAny<Func<IQueryable<Fact>, IIncludableQueryable<Fact, object>>>()))
+                .ReturnsAsync((Expression<Func<Fact, bool>> predicate, Func<IQueryable<Fact>, IIncludableQueryable<Fact, object>> include) =>
+                {
+                    return facts.FirstOrDefault(predicate.Compile());
+                });
+
+            return mockRepo;
         }
     }
 }
