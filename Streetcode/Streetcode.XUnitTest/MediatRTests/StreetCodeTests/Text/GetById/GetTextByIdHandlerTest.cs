0edf75074abd3befbfad4b13cde88af04fd55ff2
Denys Klymovets <den.kli2511@gmail.com>
2024-03-26 19:59:28 +0200

    GetTextByIdTest

--- Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCodeTests/Text/GetById/GetTextByIdHandlerTest.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCodeTests/Text/GetById/GetTextByIdHandlerTest.cs
@@ -1,4 +1,4 @@
-﻿namespace Streetcode.XUnitTest.MediatRTests.StreetCodeTests.Text.GetAll
+﻿namespace Streetcode.XUnitTest.MediatRTests.StreetCodeTests.Text.GetById
 {
     using System;
     using AutoMapper;
@@ -6,18 +6,18 @@
     using Moq;
     using Streetcode.BLL.Interfaces.Logging;
     using Streetcode.BLL.Mapping.Streetcode.TextContent;
-    using Streetcode.BLL.MediatR.Streetcode.Text.GetAll;
+    using Streetcode.BLL.MediatR.Streetcode.Text.GetById;
     using Streetcode.DAL.Repositories.Interfaces.Base;
     using Streetcode.XUnitTest.MediatRTests.Mocks;
     using Xunit;
 
-    public class GetAllTextsHandlerTest
+    public class GetTextByIdHandlerTest
     {
         private readonly IMapper mapper;
         private readonly Mock<IRepositoryWrapper> mockRepository;
         private readonly Mock<ILoggerService> mockLogger;
 
-        public GetAllTextsHandlerTest()
+        public GetTextByIdHandlerTest()
         {
             this.mockRepository = RepositoryMocker.GetAllTexts();
 
@@ -32,23 +32,13 @@
         }
 
         [Fact]
-        public async Task Result_GetAllTextsNotNull_Test()
+        public async Task Result_GetTextById_Test()
         {
-            var handler = new GetAllTextsHandler(this.mockRepository.Object, this.mapper, this.mockLogger.Object);
+            var handler = new GetTextByIdHandler(this.mockRepository.Object, this.mapper, this.mockLogger.Object);
 
-            var result = await handler.Handle(new GetAllTextsQuery(), CancellationToken.None);
+            var result = await handler.Handle(new GetTextByIdQuery(2), CancellationToken.None);
 
             result.Value.Should().NotBeNull();
-        }
-
-        [Fact]
-        public async Task Result_GetAllTextsCount_Test()
-        {
-            var handler = new GetAllTextsHandler(this.mockRepository.Object, this.mapper, this.mockLogger.Object);
-
-            var result = await handler.Handle(new GetAllTextsQuery(), CancellationToken.None);
-
-            result.Value.Count().Should().Be(3);
         }
     }
 }
