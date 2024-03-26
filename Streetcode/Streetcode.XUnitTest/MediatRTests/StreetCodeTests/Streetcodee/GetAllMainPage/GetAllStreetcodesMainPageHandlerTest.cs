59238affd4be7c394eb426e0702f93bd6af50924
Denys Klymovets <den.kli2511@gmail.com>
2024-03-26 17:18:21 +0200

    GetAllShortTest

--- Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCodeTests/Streetcodee/GetAllMainPage/GetAllStreetcodesMainPageHandlerTest.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCodeTests/Streetcodee/GetAllMainPage/GetAllStreetcodesMainPageHandlerTest.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using AutoMapper;
-using FluentAssertions;
-using Moq;
-using Streetcode.BLL.Interfaces.Logging;
-using Streetcode.BLL.Mapping.Streetcode;
-using Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAll;
-using Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAllMainPage;
-using Streetcode.DAL.Repositories.Interfaces.Base;
-using Streetcode.XUnitTest.MediatRTests.Mocks;
-using Xunit;
-
-namespace Streetcode.XUnitTest.MediatRTests.StreetCodeTests.Streetcodee.GetAllMainPage
+﻿namespace Streetcode.XUnitTest.MediatRTests.StreetCodeTests.Streetcodee.GetAllMainPage
 {
+    using System;
+    using AutoMapper;
+    using FluentAssertions;
+    using Moq;
+    using Streetcode.BLL.Interfaces.Logging;
+    using Streetcode.BLL.Mapping.Streetcode;
+    using Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAll;
+    using Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAllMainPage;
+    using Streetcode.DAL.Repositories.Interfaces.Base;
+    using Streetcode.XUnitTest.MediatRTests.Mocks;
+    using Xunit;
+
     public class GetAllStreetcodesMainPageHandlerTest
     {
         private readonly IMapper mapper;
@@ -33,7 +33,7 @@ namespace Streetcode.XUnitTest.MediatRTests.StreetCodeTests.Streetcodee.GetAllMa
         }
 
         [Fact]
-        public async Task Result_GetAllStreetcodes_Test()
+        public async Task Result_GetAllStreetcodesMainPage_Test()
         {
             var handler = new GetAllStreetcodesMainPageHandler(this.mockRepository.Object, this.mapper, this.mockLogger.Object);
 
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCodeTests/Streetcodee/GetAllShort/GetAllStreetcodesShortHandlerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Mapping.Streetcode;
+using Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAllMainPage;
+using Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAllShort;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.XUnitTest.MediatRTests.Mocks;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.StreetCodeTests.Streetcodee.GetAllShort
+{
+    public class GetAllStreetcodesShortHandlerTest
+    {
+        private readonly IMapper mapper;
+        private readonly Mock<IRepositoryWrapper> mockRepository;
+        private readonly Mock<ILoggerService> mockLogger;
+
+        public GetAllStreetcodesShortHandlerTest()
+        {
+            this.mockRepository = RepositoryMocker.GetStreetcodeRepositoryMock();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<StreetcodeProfile>();
+            });
+
+            this.mapper = mapperConfig.CreateMapper();
+
+            this.mockLogger = new Mock<ILoggerService>();
+        }
+
+        [Fact]
+        public async Task Result_GetAllStreetcodesShort_Test()
+        {
+            var handler = new GetAllStreetcodesShortHandler(this.mockRepository.Object, this.mapper, this.mockLogger.Object);
+
+            var result = await handler.Handle(new GetAllStreetcodesShortQuery(), CancellationToken.None);
+
+            result.Value.Should().NotBeNull();
+        }
+    }
+}
