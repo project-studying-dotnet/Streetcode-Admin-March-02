02087a33cffb4c7e1a013d09aa611b29b0e6f75d
ccotae <ccotae1@gmail.com>
2024-03-28 00:55:59 +0200

    add validator for timeline folder

---
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/Update/UpdateTimelineItemCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+
+namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.Update
+{
+    internal class UpdateTimelineItemCommandValidator : AbstractValidator<UpdateTimelineItemCommand>
+    {
+        private readonly int _titleMaxLength;
+        private readonly int _descriptionMaxLength;
+
+        public UpdateTimelineItemCommandValidator()
+        {
+            _titleMaxLength = 100;
+            _descriptionMaxLength = 600;
+
+            RuleFor(command => command.TimelineItem.Date)
+                .NotEmpty()
+                .WithMessage("Date is required.");
+
+            RuleFor(command => command.TimelineItem.DateViewPattern)
+                .NotEmpty()
+                .WithMessage("DataViewPattern is required.");
+
+            RuleFor(command => command.TimelineItem.Title)
+                .NotEmpty()
+                .WithMessage("Title is required.")
+                .MaximumLength(_titleMaxLength)
+                .WithMessage($"Title length must not be longer than {_titleMaxLength} symbols.");
+
+            RuleFor(command => command.TimelineItem.Description)
+                .MaximumLength(_descriptionMaxLength)
+                .WithMessage($"Title length must not be longer than {_descriptionMaxLength} symbols.");
+        }
+    }
+}
