89b54edd1302a47bb52f7055f33837e47197c879
ccotae <ccotae1@gmail.com>
2024-03-28 01:07:20 +0200

    add validator for payment

---
+++ Streetcode/Streetcode.BLL/MediatR/Payment/CreateInvoiceCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Streetcode.BLL.MediatR.Payment
+{
+    internal class CreateInvoiceCommandValidator : AbstractValidator<CreateInvoiceCommand>
+    {
+        public CreateInvoiceCommandValidator()
+        {
+            RuleFor(command => command.Payment.Amount)
+                .NotEmpty()
+                .WithMessage("Amount is required.");
+        }
+    }
+}
